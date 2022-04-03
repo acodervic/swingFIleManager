763e71797f1901f453103463eb62c268e428de81
acodervic <you@adad.com>
2022-04-03 23:34:33 +0800

    修复编译问题

--- packageBin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=$JAVA17_HOME
-mvn package
-cp  target/filemanager-1.0-SNAPSHOT.jar /home/w/mytool/filemanager/
-
-#cd /home/w/mytool/filemanager/ &&$JAVA17_HOME/bin/native-image --report-unsupported-elements-at-runtime -jar  filemanager-1.0-SNAPSHOT.jar
